208486f29b63c00d7c7e0bf0f57ababe811dfbf3
dgey <tonymande@gmx.de>
2020-05-05 08:14:37 +0200

    initial version

--- src/main/java/org/thankthemaker/quarkus/ExampleResource.java
+++ src/main/java/org/thankthemaker/quarkus/ExampleResource.java
@@ -4,8 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.thankthemaker.quarkus.logging.Traced;
 
 @Path("/hello")
+@Traced
 public class ExampleResource {
 
     @GET
