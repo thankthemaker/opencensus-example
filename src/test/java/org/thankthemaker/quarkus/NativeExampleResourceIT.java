f06a600cea2715537555cb57867f99230bc31fd9
quarkusio <no-reply@quarkus.io>
2020-05-05 06:04:07 +0000

    Initial commit

---
+++ src/test/java/org/thankthemaker/quarkus/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package org.thankthemaker.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
